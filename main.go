62ed383006ea7478d218da80c8dd12d6ca506570
Evgeny Rozaliev <rozaliev@gmail.com>
2014-11-08 23:50:32 +0300

    touch resp

--- main.go
+++ main.go
@@ -2,79 +2,121 @@ package main
 
 import (
 	"log"
+
+	"runtime"
 	"strconv"
 	"time"
 )
 
 func main() {
 	log.Println("Starting")
+	runtime.GOMAXPROCS(2)
 
-	motor, err := FindMiniTacho()
+	leftEngine, err := FindDevice("/sys/class/tacho-motor", "port_name", "outC")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Left engine not found:", err)
 	}
 
-	FatalOnErr(SetValue(motor.Path, "position", "0"))
-	FatalOnErr(SetValue(motor.Path, "run_mode", "position"))
-	FatalOnErr(SetValue(motor.Path, "stop_mode", "hold"))
-	FatalOnErr(SetValue(motor.Path, "regulation_mode", "on"))
-	FatalOnErr(SetValue(motor.Path, "ramp_up_sp", "30"))
-	FatalOnErr(SetValue(motor.Path, "ramp_down_sp", "30"))
-	FatalOnErr(SetValue(motor.Path, "pulses_per_second_sp", "1000"))
+	rightEngine, err := FindDevice("/sys/class/tacho-motor", "port_name", "outD")
+	if err != nil {
+		log.Fatal("Right engine not found:", err)
+	}
 
-	irPath, err := FindDevice("/sys/class/msensor", "port_name", "in3")
+	irSensor, err := FindDevice("/sys/class/msensor", "port_name", "in3")
 	if err != nil {
 		log.Fatal("IR not found:", err)
 	}
 
-	m := []int{}
-
-	for i := 0; i <= 360; i += 10 {
-		res, err := stepMeasure(motor.Path, i, irPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		m = append(m, res)
+	touchSensor, err := FindDevice("/sys/class/msensor", "port_name", "in2")
+	if err != nil {
+		log.Fatal("TOUCH not found:", err)
 	}
 
-	log.Println("Measurements:", m)
-
-	FatalOnErr(SetValue(motor.Path, "position_sp", "0"))
-
-	FatalOnErr(SetValue(motor.Path, "run", "1"))
-
-}
-
-func stepMeasure(mpath string, position int, irPath string) (int, error) {
-	if err := SetValue(mpath, "position_sp", strconv.Itoa(position)); err != nil {
-		return 0, err
-	}
-
-	if err := SetValue(mpath, "run", "1"); err != nil {
-		return 0, err
-	}
+	wasTouched := false
 
 	for {
-		val, err := GetValue(mpath, "run")
+
+	TOUCH:
+		for {
+			touched, err := GetValue(touchSensor, "value0")
+			if err != nil {
+				log.Fatal("Ir sensor error:", err)
+			}
+
+			if touched == "1" {
+				wasTouched = true
+				Run(leftEngine, rightEngine, "-100", time.Microsecond*500)
+				continue TOUCH
+			} else {
+				if wasTouched {
+					wasTouched = false
+					TurnLeft(leftEngine, rightEngine, "100", time.Microsecond*500)
+				}
+				break TOUCH
+			}
+		}
+
+		dist, err := GetValue(irSensor, "value0")
 		if err != nil {
-			return 0, err
-		}
-		if val == "0" {
-			break
+			log.Fatal("Ir sensor error:", err)
 		}
 
-		time.Sleep(5 * time.Millisecond)
+		distInt, err := strconv.Atoi(dist)
+		if err != nil {
+			log.Fatal("Ir sensor value error:", err)
+		}
+
+		if distInt < 60 {
+			Stop(leftEngine, rightEngine)
+			TurnLeft(leftEngine, rightEngine, "100", time.Millisecond*500)
+		} else {
+			RunInf(leftEngine, rightEngine, "100")
+		}
+
+		time.Sleep(time.Millisecond * 20)
 	}
+}
 
-	time.Sleep(10 * time.Millisecond)
+func Run(leftEngine, rightEngine, speed string, timeout time.Duration) {
+	FatalOnErr(SetValue(leftEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(rightEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(leftEngine, "run", "1"))
+	FatalOnErr(SetValue(rightEngine, "run", "1"))
+	<-time.After(timeout)
+	FatalOnErr(SetValue(leftEngine, "run", "0"))
+	FatalOnErr(SetValue(rightEngine, "run", "0"))
+}
 
-	val, err := GetValue(irPath, "value0")
-	if err != nil {
-		return 0, err
-	}
+func RunInf(leftEngine, rightEngine, speed string) {
+	FatalOnErr(SetValue(leftEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(rightEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(leftEngine, "run", "1"))
+	FatalOnErr(SetValue(rightEngine, "run", "1"))
+}
 
-	return strconv.Atoi(val)
+func Stop(leftEngine, rightEngine string) {
+	FatalOnErr(SetValue(leftEngine, "run", "0"))
+	FatalOnErr(SetValue(rightEngine, "run", "0"))
+}
+
+func TurnLeft(leftEngine, rightEngine, speed string, timeout time.Duration) {
+	FatalOnErr(SetValue(leftEngine, "duty_cycle_sp", "-"+speed))
+	FatalOnErr(SetValue(rightEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(leftEngine, "run", "1"))
+	FatalOnErr(SetValue(rightEngine, "run", "1"))
+	<-time.After(timeout)
+	FatalOnErr(SetValue(leftEngine, "run", "0"))
+	FatalOnErr(SetValue(rightEngine, "run", "0"))
+}
+
+func TurnRight(leftEngine, rightEngine, speed string, timeout time.Duration) {
+	FatalOnErr(SetValue(leftEngine, "duty_cycle_sp", speed))
+	FatalOnErr(SetValue(rightEngine, "duty_cycle_sp", "-"+speed))
+	FatalOnErr(SetValue(leftEngine, "run", "1"))
+	FatalOnErr(SetValue(rightEngine, "run", "1"))
+	<-time.After(timeout)
+	FatalOnErr(SetValue(leftEngine, "run", "0"))
+	FatalOnErr(SetValue(rightEngine, "run", "0"))
 }
 
 func FatalOnErr(err error) {
